3b8f751bea1560b91c1cc4b6782a1281d84cc574
Janis Lebron <janislebron@gmail.com>
2021-01-16 02:07:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,9 @@ class Scraper
     
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
       binding.pry
-    end
+  end
   
     def get_courses
     end
